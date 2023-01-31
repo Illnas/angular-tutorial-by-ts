3bf4448332c47ce5e78974281fd7f3d842f3d36e
Nadatari <dariosting@gmail.com>
2023-01-31 21:32:15 +0100

    Tutorial started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface Pokemon {
+  id: number,
+  name: string,
+  type: string,
+  isCool: boolean,
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title: string;
+  numberOne: number = 1;
+  togglePokemon: boolean = false;
+
+  titleSecond: string = "basker";
+  imgSrc: string = "https://media.istockphoto.com/id/842493890/photo/beaver.jpg?s=612x612&w=0&k=20&c=UcBraaIug4HiBU7_cVbhwXIg0GLitSbrgrLDvSOowqo="
+
+  favoriteAnimal: string = "turtle";
+  pokemonName: string = "";
+
+  pokemons: Pokemon[] = [{
+    id: 1,
+    name: 'pikachu',
+    type: 'electric',
+    isCool: false
+  },
+  {
+    id: 2,
+    name: 'squirtle',
+    type: 'water',
+    isCool: true
+  },
+  {
+    id: 3,
+    name: 'charmander',
+    type: 'fire',
+    isCool: false
+  }]
+
+
+  constructor() {
+    this.title = "Nada"
+  }
+
+  handleClick(value: any) {
+    console.log(value);
+  }
 }
