fa8e131c470afa4ae7476dabd36962c769b99324
Nadatari <dariosting@gmail.com>
2023-02-07 21:51:19 +0100

    Lesson on ngOnInit done

--- src/app/pokemon-base/pokemon-base.module.ts
+++ src/app/pokemon-base/pokemon-base.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PokemonListComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    PokemonListComponent
   ]
 })
 export class PokemonBaseModule { }
